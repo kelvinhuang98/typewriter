8f00b948ff53e95e23c2038bf05461cbf2d31c9f
kelvinhuang98 <kelvin.huang98@hotmail.com>
2022-10-25 16:17:53 -0400

    Added new line after finishing the sentence

--- typewriter.js
+++ typewriter.js
@@ -1,9 +1,15 @@
-const sentence = "hello there from lighthouse labs\n";
+const sentence = "hello there from lighthouse labs";
 
 let delay = 0;
-for (const char of sentence) {
-  setTimeout(() => {
-    process.stdout.write(char);
-  }, delay);
-  delay += 50;
+for (let i = 0; i <= sentence.length; i++) {
+  if (i < sentence.length) {
+    setTimeout(() => {
+      process.stdout.write(sentence[i]);
+    }, delay);
+    delay += 50;
+  } else {
+    setTimeout(() => {
+      process.stdout.write(`\n`);
+    }, delay);
+  }
 }
